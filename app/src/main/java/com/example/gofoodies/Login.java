4d6e087fbee662639a82b664b833481440003380
Shivaji-Paudel <94693106+Shivaji-Paudel@users.noreply.github.com>
2022-10-07 18:15:06 +0530

    Add login and registration activities

--- app/src/main/java/com/example/gofoodies/Login.java
+++ app/src/main/java/com/example/gofoodies/Login.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SplashScreenActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_login);
     }
 }
--- app/src/main/java/com/example/gofoodies/sign_up.java
+++ app/src/main/java/com/example/gofoodies/sign_up.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class sign_up extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
     }
 }

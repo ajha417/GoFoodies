e2331fcb2dc665ac399bbcb36a8ac7b8dbcb8d5a
Shivaji-Paudel <94693106+Shivaji-Paudel@users.noreply.github.com>
2022-10-07 18:19:10 +0530

    Add login and registration activities

--- app/src/main/java/com/example/gofoodies/activities/Login.java
+++ app/src/main/java/com/example/gofoodies/activities/Login.java
@@ -1,8 +1,10 @@
-package com.example.gofoodies;
+package com.example.gofoodies.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.gofoodies.R;
 
 public class Login extends AppCompatActivity {
 
